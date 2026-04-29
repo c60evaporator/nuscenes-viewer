58f2da083918d7f3a9e26fe18293e11de5b65855
knakamura <c60evaporator@gmail.com>
2026-04-29 22:29:20 +0900

    Small modification

--- frontend/src/types/annotation.ts
+++ frontend/src/types/annotation.ts
@@ -1,15 +1,5 @@
 import type { Point3D, Quaternion, Dimensions3D } from './common'
 
-export interface BoundingBox3D {
-  token:      string
-  center:     Point3D
-  dimensions: Dimensions3D
-  rotation:   Quaternion
-  category:   string
-  attributes: string[]
-  velocity:   Point3D | null
-}
-
 export interface AnnotationUpdateRequest {
   center?:     Point3D
   dimensions?: Dimensions3D

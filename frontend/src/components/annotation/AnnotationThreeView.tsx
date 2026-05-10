68ae53a06899fed1a41388a79bf98b8451906cec
knakamura <c60evaporator@gmail.com>
2026-05-10 18:17:21 +0900

    Make the 3D VIEW BBox lines thicker

--- frontend/src/components/annotation/AnnotationThreeView.tsx
+++ frontend/src/components/annotation/AnnotationThreeView.tsx
@@ -1,6 +1,6 @@
 import { useMemo } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, Line } from '@react-three/drei'
 import { usePointCloud } from '@/api/sensorData'
 import { useEditStore } from '@/store/editStore'
 import { bboxCornersToGlobal, globalToSensor } from '@/lib/coordinateUtils'
@@ -59,8 +59,10 @@ export default function AnnotationThreeView({
                     far:      1000,
                 }}
             >
-                {/* 座標軸: X=赤, Y=緑, Z=青、長さ10m */}
-                <axesHelper args={[10]} />
+                {/* 座標軸: X=赤(前), Y=緑(左), Z=青(上)、長さ10m */}
+                <Line points={[[0,0,0],[10,0,0]]} color='red'   lineWidth={1} />
+                <Line points={[[0,0,0],[0,10,0]]} color='green' lineWidth={1} />
+                <Line points={[[0,0,0],[0,0,10]]} color='blue'  lineWidth={1} />
 
                 {/* グリッド (XY平面, 5m間隔 = 100m / 20分割) */}
                 <gridHelper
@@ -83,6 +85,7 @@ export default function AnnotationThreeView({
                             : ann.instance_token === highlightInstanceToken ? '#FFD700'
                             : '#00FF88'
                         }
+                        lineWidth={ann.instance_token === editingInstanceToken ? 2 : 1.5}
                         onClick={onBBoxClick}
                     />
                 ))}
@@ -138,15 +141,16 @@ function PointCloudMesh({ points }: { points: number[][] }) {
 }
 
 function BBoxMesh({
-    ann, egoPose, lidarCalibSensor, color, onClick,
+    ann, egoPose, lidarCalibSensor, color, lineWidth, onClick,
 }: {
     ann:              Annotation
     egoPose:          EgoPosePoint
     lidarCalibSensor: { translation: number[]; rotation: number[] }
     color:            string
+    lineWidth:        number
     onClick?:         (annToken: string) => void
 }) {
-    const { lineVertices, aabbCenter, aabbSize } = useMemo(() => {
+    const { edgePoints, aabbCenter, aabbSize } = useMemo(() => {
         // 8隅をセンサー座標系で計算
         const globalCorners = bboxCornersToGlobal(ann.translation, ann.rotation, ann.size)
         const sensorCorners = globalCorners.map((c) => globalToSensor(c, egoPose, lidarCalibSensor))
@@ -162,14 +166,10 @@ function BBoxMesh({
             [2, 3], [3, 7], [7, 6], [6, 2],   // 下面
             [0, 3], [1, 2], [4, 7], [5, 6],   // 縦辺
         ]
-        const lineVertices = new Float32Array(edges.length * 6)
-        edges.forEach(([a, b], i) => {
-            lineVertices[i * 6]     = displayCorners[a][0]
-            lineVertices[i * 6 + 1] = displayCorners[a][1]
-            lineVertices[i * 6 + 2] = displayCorners[a][2]
-            lineVertices[i * 6 + 3] = displayCorners[b][0]
-            lineVertices[i * 6 + 4] = displayCorners[b][1]
-            lineVertices[i * 6 + 5] = displayCorners[b][2]
+        const edgePoints: [number, number, number][] = []
+        edges.forEach(([a, b]) => {
+            edgePoints.push([displayCorners[a][0], displayCorners[a][1], displayCorners[a][2]])
+            edgePoints.push([displayCorners[b][0], displayCorners[b][1], displayCorners[b][2]])
         })
 
         // クリック判定用 AABB (軸並行bounding box)
@@ -188,21 +188,13 @@ function BBoxMesh({
             Math.max(maxZ - minZ, 0.1),
         ]
 
-        return { lineVertices, aabbCenter, aabbSize }
+        return { edgePoints, aabbCenter, aabbSize }
     }, [ann, egoPose, lidarCalibSensor])
 
     return (
         <group>
-            {/* ワイヤーフレーム */}
-            <lineSegments>
-                <bufferGeometry>
-                    <bufferAttribute
-                        attach='attributes-position'
-                        args={[lineVertices, 3]}
-                    />
-                </bufferGeometry>
-                <lineBasicMaterial color={color} />
-            </lineSegments>
+            {/* ワイヤーフレーム (drei Line で太さ指定) */}
+            <Line points={edgePoints} color={color} lineWidth={lineWidth} segments />
 
             {/* クリック判定用 (透明) */}
             {onClick && (
